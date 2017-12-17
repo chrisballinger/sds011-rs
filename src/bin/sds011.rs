588f024c913c7591ad9c0266ec297d6fd822cdc3
Chris Ballinger <chrisballinger@gmail.com>
2017-12-17 13:43:56 -0800

    Write output csv to file

--- src/bin/sds011.rs
+++ src/bin/sds011.rs
@@ -1,13 +1,14 @@
 extern crate sds011;
 extern crate clap;
+extern crate chrono;
+extern crate csv;
 
 use clap::{App,Arg};
 use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::time::Duration;
 use sds011::*;
-
-extern crate csv;
+use chrono::prelude::*;
 
 
 const OUTPUT_DIR: &str = "OUTPUT_DIR";
@@ -43,9 +44,9 @@ fn main() {
     };
 
     let out_dir_ref = env::current_dir().unwrap();
-    let out_dir: &Path = match matches.value_of(OUTPUT_DIR) {
-        Some(path) =>  Path::new(path),
-        None => out_dir_ref.as_path()
+    let out_dir: PathBuf = match matches.value_of(OUTPUT_DIR) {
+        Some(path) =>  PathBuf::from(path),
+        None => out_dir_ref
     };
 
     println!("Output file directory: {:?}", out_dir);
@@ -59,15 +60,22 @@ fn main() {
     sensor.configure(Duration::from_secs(1));
     println!("Configured device");
 
-
-    //let output = sensor.read_bytes(10).unwrap();
-    //println!("Read bytes: {:?}", output);
-
     let response = sensor.get_sensor_info().unwrap();
-    println!("response bytes: {:?}", response);
+    println!("Got sensor info");
+
+    let start_time = Utc::now();
+    let start_time_string = start_time.format("%Y-%m-%d %H_%M_%S").to_string();
+    let mut output_file_path = out_dir.clone();
+    output_file_path.push(start_time_string);
+    output_file_path.set_extension("csv");
+
+    println!("Recording measurements to file: {:?}", output_file_path);
+    let mut csv_writer = csv::Writer::from_path(output_file_path.as_path()).unwrap();
 
     for i in 0..100 {
         let measurement = sensor.get_measurement().unwrap();
         println!("measurement: {:?}", measurement);
+        csv_writer.serialize(measurement);
+        csv_writer.flush();
     }
 }
